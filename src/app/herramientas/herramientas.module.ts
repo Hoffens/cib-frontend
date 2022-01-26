2632c5a0641c5511c74e9bf1c267f35d31d65c7c
Nicolas Fernandez <nicolas.fernandez@dcore.cl>
2022-01-26 01:16:40 -0300

    creacion de componente herramientas

---
+++ src/app/herramientas/herramientas.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HerramientasComponent } from './pages/herramientas/herramientas.component';
+
+
+
+@NgModule({
+  declarations: [
+    HerramientasComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HerramientasModule { }
