441576916975fcc50a0ccc84478e6d6322556b40
Nicolas Fernandez <nicolas.fernandez@dcore.cl>
2022-01-26 04:12:53 -0300

    modal creacion herramientas

--- src/app/herramientas/pages/herramientas/herramientas.component.ts
+++ src/app/herramientas/pages/herramientas/herramientas.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 import { HerramientasService } from '../../services/herramientas.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-herramientas',
@@ -9,15 +12,43 @@ import { HerramientasService } from '../../services/herramientas.service';
 })
 export class HerramientasComponent implements OnInit {
 
+  herramientaForm = new FormGroup({
+    serie: new FormControl('', Validators.required),
+    compania: new FormControl(0, Validators.required),
+    carro: new FormControl('', Validators.required),
+    nombre: new FormControl('', Validators.required),
+    descripcion: new FormControl('', Validators.required),
+    tipo: new FormControl(0, Validators.required),
+    activo: new FormControl(true)
+  });
+
   public actualUser: any
   public listHerramientas: any = []
+  public listCompanias: any = []
+  public listCarros: any = []
+  public listTipos: any = []
   public isLoading: boolean = true
   public showMenu: boolean = true
+  public ngbModalOptions: NgbModalOptions = {
+    backdrop : 'static',
+    keyboard : false,
+    scrollable : true,
+    size : 'lg'
+  }
+  public modalReference: any = null
+  public errorFormulario: boolean = false
+  public mostrarFail: boolean = false
+  public mostrarExito: boolean = false
+  public errorMsg: string = ''
 
-  constructor(private herramientasService: HerramientasService, private route: Router) { }
+
+  constructor(private herramientasService: HerramientasService, private route: Router, private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.validarToken()
+    this.obtenerCompanias()
+    this.obtenerCarros()
+    this.obtenerTipos()
     let usuario = localStorage.getItem('usuario')
     this.actualUser = JSON.parse(usuario == null ? '' : usuario)
     this.obtenerHerramientas()
@@ -71,6 +102,127 @@ export class HerramientasComponent implements OnInit {
         this.isLoading = false
       }
     })
+  }
+
+  obtenerCompanias() {
+    let token = localStorage.getItem('token') 
+
+    this.herramientasService.obtenerCompanias(token == null ? '' : token).subscribe({
+      next: (v: any) => { 
+        this.listCompanias = v.data
+        console.log(this.listCompanias)
+        this.isLoading = false
+      },
+      error: (e) => {
+        console.log(e)
+        this.isLoading = false
+      }
+    })
+  }
+
+  obtenerCarros() {
+    let token = localStorage.getItem('token') 
+
+    this.herramientasService.obtenerCarros(token == null ? '' : token).subscribe({
+      next: (v: any) => { 
+        this.listCarros = v.data
+        console.log(this.listCarros)
+        this.isLoading = false
+      },
+      error: (e) => {
+        console.log(e)
+        this.isLoading = false
+      }
+    })
+  }
+
+  obtenerTipos() {
+    let token = localStorage.getItem('token') 
+
+    this.herramientasService.obtenerTipo(token == null ? '' : token).subscribe({
+      next: (v: any) => { 
+        this.listTipos = v.data
+        console.log(this.listTipos)
+        this.isLoading = false
+      },
+      error: (e) => {
+        console.log(e)
+        this.isLoading = false
+      }
+    })
+  }
+
+  open(content: any) {
+    this.modalReference = this.modalService.open(content, this.ngbModalOptions)
+  }
+
+  limpiarFormulario() {
+    this.herramientaForm = new FormGroup({
+      serie: new FormControl('', Validators.required),
+      compania: new FormControl(0, Validators.required),
+      carro: new FormControl('', Validators.required),
+      nombre: new FormControl('', Validators.required),
+      descripcion: new FormControl('', Validators.required),
+      tipo: new FormControl(0, Validators.required),
+      activo: new FormControl(true)
+    });
+  }
+
+  onSubmit() {
+    let token = localStorage.getItem('token') 
+    if (this.herramientaForm.valid) {
+      this.errorFormulario = false
+
+      const payload = {
+        serie: this.herramientaForm.get('serie')?.value,
+        compania: Number(this.herramientaForm.get('compania')?.value),
+        carro: this.herramientaForm.get('carro')?.value,
+        nombre: this.herramientaForm.get('nombre')?.value,
+        descripcion: this.herramientaForm.get('descripcion')?.value,
+        tipo: Number(this.herramientaForm.get('tipo')?.value),
+        activo: true,
+        rut_cuenta: this.actualUser.rut
+      }
+
+      console.log(JSON.stringify(payload))
+
+      this.herramientasService.crearHerramienta(token == null ? '' : token, payload).subscribe({
+        next: (v: any) => {
+          this.limpiarFormulario()
+          this.mostrarFail = false
+          this.mostrarExito = true
+          this.modalReference.close()
+        },
+        error: (e: any) => {
+          console.log(e)
+          this.mostrarExito = false
+          this.mostrarFail = true
+          this.errorMsg = e.error.message
+          this.modalReference.close()
+        }
+      })
+
+    } else {
+      this.errorFormulario = true
+    }
+  }
+
+  closeErrorAlert() {
+    this.mostrarFail = false
+  }
+
+  closeSuccessAlert() {
+    this.mostrarExito = false
+  }
+
+  closeModalErrorAlert() {
+    this.errorFormulario = false
+  }
+  
+  onCancel() {
+    this.limpiarFormulario()
+    this.errorFormulario = false
+    this.modalReference.close()
   }
 
 }
--- src/app/herramientas/services/herramientas.service.ts
+++ src/app/herramientas/services/herramientas.service.ts
@@ -9,6 +9,10 @@ export class HerramientasService {
 
   private endpoint: string = '/api/herramientas'
   private endpointToken: string = '/api/validateToken'
+  private endpointCompanias: string = '/api/companias'
+  private endpointCarros: string = '/api/carros'
+  private endpointTipo: string = '/api/tipos_de_herramientas'
+  private endpointCrear: string = '/api/herramienta'
 
 
   constructor(private http: HttpClient) { }
@@ -19,6 +23,22 @@ export class HerramientasService {
 
   validarToken(token: string) {
     return this.http.get(this.endpointToken, { headers: {'Authorization' : token} })
+  }
+
+  obtenerCompanias(token: string) {
+    return this.http.get(this.endpointCompanias, { headers: {'Authorization' : token} })
+  }
+
+  obtenerCarros(token: string) {
+    return this.http.get(this.endpointCarros, { headers: {'Authorization' : token} })
+  }
+
+  obtenerTipo(token: string) {
+    return this.http.get(this.endpointTipo, { headers: {'Authorization' : token} })
+  }
+
+  crearHerramienta(token: string, payload: any) {
+    return this.http.post(this.endpointCrear, payload, { headers: {'Authorization' : token} })
   }
 
 }
