e3a031bd6641d66928b3178f48e0ed93ffb49684
Nicolas Fernandez <nicolas.fernandez@dcore.cl>
2022-01-26 02:08:11 -0300

    funciones necesarias para traer los informes desde la api

--- src/app/informe/pages/informe/informe.component.ts
+++ src/app/informe/pages/informe/informe.component.ts
@@ -9,9 +9,68 @@ import { InformeService } from '../../services/informe.service';
 })
 export class InformeComponent implements OnInit {
 
+  public actualUser: any
+  public listInformes: any = []
+  public isLoading: boolean = true
+  public showMenu: boolean = true
+
   constructor(private informeService: InformeService, private route: Router) { }
 
   ngOnInit(): void {
+    this.validarToken()
+    let usuario = localStorage.getItem('usuario')
+    this.actualUser = JSON.parse(usuario == null ? '' : usuario)
+    this.obtenerInformes()
+  }
+
+  validarToken() {
+    let token = localStorage.getItem('token')
+    if (token) {
+      this.informeService.validarToken(token).subscribe({
+        error: (e) => {
+          this.route.navigate(['/404'])
+        }
+      })
+    }
+    else
+    {
+      this.route.navigate(['/404'])
+    }
+  }
+
+  esconderMenu() {
+    //this.showMenu ? this.showMenu = false : this.showMenu = true
+    let sidebar = document.getElementById('sidebar')
+    this.showMenu ? this.showMenu = false : this.showMenu = true
+    /*
+    if (this.showMenu) {
+      if (sidebar != null)
+          sidebar.setAttribute('style', 'margin-left: -250px;')
+      this.showMenu = false
+    }
+    else
+    {
+      if (sidebar != null)
+          sidebar.setAttribute('style', 'margin-left: 0;')
+      this.showMenu = true
+    }
+    */
+  }
+
+  obtenerInformes() {
+    let token = localStorage.getItem('token') 
+
+    this.informeService.obtenerInformes(token == null ? '' : token).subscribe({
+      next: (v: any) => { 
+        this.listInformes = v.data
+        console.log(this.listInformes)
+        this.isLoading = false
+      },
+      error: (e) => {
+        console.log(e)
+        this.isLoading = false
+      }
+    })
   }
 
 }
