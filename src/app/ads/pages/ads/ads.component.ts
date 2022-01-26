9869d00119a3a7895a289de5fc091f8d2051b6e6
Nicolas Fernandez <nicolas.fernandez@dcore.cl>
2022-01-26 01:59:25 -0300

    funciones necesarias para traer los ads desde la api

--- src/app/ads/pages/ads/ads.component.ts
+++ src/app/ads/pages/ads/ads.component.ts
@@ -9,9 +9,68 @@ import { AdsService } from '../../services/ads.service';
 })
 export class AdsComponent implements OnInit {
 
+  public actualUser: any
+  public listAds: any = []
+  public isLoading: boolean = true
+  public showMenu: boolean = true
+
   constructor(private adsService: AdsService, private route: Router) { }
 
   ngOnInit(): void {
+    this.validarToken()
+    let usuario = localStorage.getItem('usuario')
+    this.actualUser = JSON.parse(usuario == null ? '' : usuario)
+    this.obtenerAds()
+  }
+
+  validarToken() {
+    let token = localStorage.getItem('token')
+    if (token) {
+      this.adsService.validarToken(token).subscribe({
+        error: (e) => {
+          this.route.navigate(['/404'])
+        }
+      })
+    }
+    else
+    {
+      this.route.navigate(['/404'])
+    }
+  }
+
+  esconderMenu() {
+    //this.showMenu ? this.showMenu = false : this.showMenu = true
+    let sidebar = document.getElementById('sidebar')
+    this.showMenu ? this.showMenu = false : this.showMenu = true
+    /*
+    if (this.showMenu) {
+      if (sidebar != null)
+          sidebar.setAttribute('style', 'margin-left: -250px;')
+      this.showMenu = false
+    }
+    else
+    {
+      if (sidebar != null)
+          sidebar.setAttribute('style', 'margin-left: 0;')
+      this.showMenu = true
+    }
+    */
+  }
+
+  obtenerAds() {
+    let token = localStorage.getItem('token') 
+
+    this.adsService.obtenerAds(token == null ? '' : token).subscribe({
+      next: (v: any) => { 
+        this.listAds = v.data
+        console.log(this.listAds)
+        this.isLoading = false
+      },
+      error: (e) => {
+        console.log(e)
+        this.isLoading = false
+      }
+    })
   }
 
 }
