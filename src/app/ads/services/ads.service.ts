37239f1076c9d5f4d5791f35f415e385ca25c172
Nicolas Fernandez <nicolas.fernandez@dcore.cl>
2022-01-26 01:55:45 -0300

    se agregan servicios de ads

--- src/app/ads/services/ads.service.ts
+++ src/app/ads/services/ads.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdsService {
 
-  constructor() { }
+  private endpoint: string = '/api/actos'
+  private endpointToken: string = '/api/validateToken'
+
+  constructor(private http: HttpClient) { }
+
+  obtenerAds(token: string) {
+    return this.http.get(this.endpoint, { headers: {'Authorization' : token} })
+  }
+
+  validarToken(token: string) {
+    return this.http.get(this.endpointToken, { headers: {'Authorization' : token} })
+  }
+
 }
