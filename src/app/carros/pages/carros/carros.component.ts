10c0da4cfee1def99dcc39f2ff0c130976e77ad5
Nicolas Fernandez <nicolas.fernandez@dcore.cl>
2022-01-26 00:53:30 -0300

    creacion de carros

--- src/app/carros/pages/carros/carros.component.ts
+++ src/app/carros/pages/carros/carros.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CarrosService } from '../../services/carros.service';
 import { Router } from '@angular/router';
+import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-carros',
@@ -9,18 +12,106 @@ import { Router } from '@angular/router';
 })
 export class CarrosComponent implements OnInit {
 
+  carroForm = new FormGroup({
+    patente: new FormControl('', Validators.required),
+    compania: new FormControl(0, Validators.required),
+    tipo_carro: new FormControl(0, Validators.required),
+    marca: new FormControl(0, Validators.required),
+    modelo: new FormControl(0, Validators.required),
+    anio_fabricacion: new FormControl(null),
+    siguiente_mantencion: new FormControl('', Validators.required),
+    activo: new FormControl(true)
+  });
+  public ngbModalOptions: NgbModalOptions = {
+    backdrop : 'static',
+    keyboard : false,
+    scrollable : true,
+    size : 'lg'
+  }
+  public listCompanias: any = []
   public listCarros: any = []
+  public listTipos: any = []
+  public listMarcas: any = []
+  public listModelos: any = []
+  public listModelosMarca: any = []
   public isLoading: boolean = true
   public actualUser: any
   public showMenu: boolean = true
+  public modalReference: any = null
+  public errorFormulario: any = false
+  public mostrarFail: any = false
+  public mostrarExito: any = false
+  public errorMsg: string = ''
 
-  constructor(private carroService: CarrosService, private route: Router) { }
+
+  constructor(private carroService: CarrosService, private route: Router, private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.validarToken()
+    this.obtenerCompanias()
+    this.obtenerTipos()
+    this.obtenerMarcas()
+    this.obtenerModelos()
     let usuario = localStorage.getItem('usuario')
     this.actualUser = JSON.parse(usuario == null ? '' : usuario)
     this.obtenerCarros()
+  }
+
+  obtenerCompanias() {
+    let token = localStorage.getItem('token') 
+
+    this.carroService.obtenerCompanias(token == null ? '' : token).subscribe({
+      next: (v: any) => { 
+        this.listCompanias = v.data
+        this.isLoading = false
+      },
+      error: (e) => {
+        console.log(e)
+        this.isLoading = false
+      }
+    })
+  }
+
+  obtenerTipos() {
+    let token = localStorage.getItem('token') 
+    this.carroService.obtenerTipos(token == null ? '' : token).subscribe({
+      next: (v: any) => { 
+        this.listTipos = v.data
+        this.isLoading = false
+      },
+      error: (e) => {
+        console.log(e)
+        this.isLoading = false
+      }
+    })
+  }
+
+  obtenerMarcas() {
+    let token = localStorage.getItem('token') 
+    this.carroService.obtenerMarcas(token == null ? '' : token).subscribe({
+      next: (v: any) => { 
+        this.listMarcas = v.data
+        this.isLoading = false
+      },
+      error: (e) => {
+        console.log(e)
+        this.isLoading = false
+      }
+    })
+  }
+
+  obtenerModelos() {
+    let token = localStorage.getItem('token') 
+    this.carroService.obtenerModelos(token == null ? '' : token).subscribe({
+      next: (v: any) => { 
+        this.listModelos = v.data
+        this.isLoading = false
+      },
+      error: (e) => {
+        console.log(e)
+        this.isLoading = false
+      }
+    })
   }
 
   obtenerCarros() {
@@ -28,7 +119,6 @@ export class CarrosComponent implements OnInit {
     this.carroService.obtenerCarros(token == null ? '' : token).subscribe({
       next: (v: any) => { 
         this.listCarros = v.data
-        console.log(this.listCarros)
         this.isLoading = false
       },
       error: (e) => {
@@ -57,6 +147,18 @@ export class CarrosComponent implements OnInit {
     */
   }
 
+  onMarcaChange(marca: any) {
+    this.listModelosMarca = []
+    for (let i = 0; i < this.listModelos.length; i++) {
+      if (this.listModelos[i].marca_id == marca)
+        this.listModelosMarca.push({
+          id : this.listModelos[i].id,
+          nombre : this.listModelos[i].nombre,
+          anio : this.listModelos[i].anio
+        })
+    }
+  }
+
   validarToken() {
     let token = localStorage.getItem('token')
     if (token) {
@@ -70,6 +172,80 @@ export class CarrosComponent implements OnInit {
     {
       this.route.navigate(['/404'])
     }
+  }
+
+  limpiarFormulario() {
+    this.carroForm = new FormGroup({
+      patente: new FormControl('', Validators.required),
+      compania: new FormControl(0, Validators.required),
+      tipo_carro: new FormControl(0, Validators.required),
+      marca: new FormControl(0, Validators.required),
+      modelo: new FormControl(0, Validators.required),
+      anio_fabricacion: new FormControl(null),
+      siguiente_mantencion: new FormControl('', Validators.required),
+      activo: new FormControl(true)
+    });
+  }
+
+  open(content: any) {
+    this.modalReference = this.modalService.open(content, this.ngbModalOptions)
+  }
+
+  onSubmit() {
+    let token = localStorage.getItem('token') 
+
+    const payload = {
+      patente: this.carroForm.get('patente')?.value,
+      compania: Number(this.carroForm.get('compania')?.value),
+      tipo: Number(this.carroForm.get('tipo_carro')?.value),
+      marca: Number(this.carroForm.get('marca')?.value),
+      modelo: Number(this.carroForm.get('modelo')?.value),
+      anio_fabricacion: this.carroForm.get('anio_fabricacion')?.value,
+      siguiente_mantencion: this.carroForm.get('siguiente_mantencion')?.value,
+      activo: true,
+      usuario_rut: this.actualUser.rut
+    }
+
+    console.log(JSON.stringify(payload))
+    if (this.carroForm.valid) {
+      this.errorFormulario = false
+      this.carroService.crearCarro(token == null ? '' : token, payload).subscribe({
+        next: (v: any) => {
+          this.limpiarFormulario()
+          this.mostrarFail = false
+          this.mostrarExito = true
+          this.modalReference.close()
+        },
+        error: (e: any) => {
+          console.log(e)
+          this.mostrarExito = false
+          this.mostrarFail = true
+          this.errorMsg = e.error.message
+          this.modalReference.close()
+        }
+      })
+
+    } else {
+      this.errorFormulario = true
+    }
+  }
+
+  closeErrorAlert() {
+    this.mostrarFail = false
+  }
+
+  closeSuccessAlert() {
+    this.mostrarExito = false
+  }
+
+  closeModalErrorAlert() {
+    this.errorFormulario = false
+  }
+
+  onCancel() {
+    this.limpiarFormulario()
+    this.errorFormulario = false
+    this.modalReference.close()
   }
 
 }
--- src/app/carros/services/carros.service.ts
+++ src/app/carros/services/carros.service.ts
@@ -8,18 +8,40 @@ export class CarrosService {
 
   private endpoint: string = '/api/carros'
   private endpointToken: string = '/api/validateToken'
+  private endpointMarca: string = '/api/marcas_carros'
+  private endpointModelo: string = '/api/modelos_carros'
+  private endpointCompanias: string = '/api/companias'
+  private endpointTipoCarro: string = '/api/tipos_carro'
+  private endpointCrearCarro: string = '/api/carro'
+
 
   constructor(private http: HttpClient) { }
 
   obtenerCarros(token: string) {
-    //let header = new HttpHeaders()
-    //let token = JSON.stringify(localStorage.getItem('token'))
-    //let xd = localStorage.getItem('token') 
-
     return this.http.get(this.endpoint, { headers: {'Authorization' : token} })
+  }
+
+  obtenerCompanias(token: string) {
+    return this.http.get(this.endpointCompanias, { headers: {'Authorization' : token} })
+  }
+
+  obtenerTipos(token: string) {
+    return this.http.get(this.endpointTipoCarro, { headers: {'Authorization' : token} })
+  }
+
+  obtenerMarcas(token: string) {
+    return this.http.get(this.endpointMarca, { headers: {'Authorization' : token} })
+  }
+
+  obtenerModelos(token: string) {
+    return this.http.get(this.endpointModelo, { headers: {'Authorization' : token} })
   }
 
   validarToken(token: string) {
     return this.http.get(this.endpointToken, { headers: {'Authorization' : token} })
+  }
+
+  crearCarro(token: string, payload: any) {
+    return this.http.post(this.endpointCrearCarro, payload, { headers: {'Authorization' : token} })
   }
 }
