f79245eda987e304ca28ddd98b4e3900426f75f4
Nicolas Fernandez <nicolas.fernandez@dcore.cl>
2022-01-26 01:28:26 -0300

    se obtienen las herramientas desde la api

--- src/app/herramientas/pages/herramientas/herramientas.component.ts
+++ src/app/herramientas/pages/herramientas/herramientas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HerramientasService } from '../../services/herramientas.service';
 
 @Component({
   selector: 'app-herramientas',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HerramientasComponent implements OnInit {
 
-  constructor() { }
+  public actualUser: any
+  public listHerramientas: any = []
+  public isLoading: boolean = false
+
+  constructor(private herramientasService: HerramientasService, private route: Router) { }
 
   ngOnInit(): void {
+    this.validarToken()
+    let usuario = localStorage.getItem('usuario')
+    this.actualUser = JSON.parse(usuario == null ? '' : usuario)
+    this.obtenerHerramientas()
+  }
+
+  validarToken() {
+    let token = localStorage.getItem('token')
+    if (token) {
+      this.herramientasService.validarToken(token).subscribe({
+        error: (e) => {
+          this.route.navigate(['/404'])
+        }
+      })
+    }
+    else
+    {
+      this.route.navigate(['/404'])
+    }
+  }
+
+  obtenerHerramientas() {
+    let token = localStorage.getItem('token') 
+
+    this.herramientasService.obtenerHerramientas(token == null ? '' : token).subscribe({
+      next: (v: any) => { 
+        this.listHerramientas = v.data
+        console.log(this.listHerramientas)
+        this.isLoading = false
+      },
+      error: (e) => {
+        console.log(e)
+        this.isLoading = false
+      }
+    })
   }
 
 }
